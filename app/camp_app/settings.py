a2e0560a292c8a799f6d079509ae3c678fceb91c
Anton Pytel <anton.pytel@gmail.com>
2021-10-31 01:08:12 +0200

    feat: static

--- app/camp_app/settings.py
+++ app/camp_app/settings.py
@@ -183,6 +183,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
