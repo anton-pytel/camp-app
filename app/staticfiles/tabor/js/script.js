b0849694e6a940f5113536e8af629fa4f785ad2a
Anton Pytel <anton.pytel@gmail.com>
2022-03-25 22:56:03 +0100

    feat: dynamic texts + Steve changes

--- app/staticfiles/tabor/js/script.js
+++ app/staticfiles/tabor/js/script.js
@@ -1,49 +1,49 @@
+let nastimobal = document.querySelector('.nastimobal');
+var nastimclick = document.getElementById('nastimobal');
+var nastimclickz = document.getElementById('nastimobalz');
+nastimclick.onclick = function () {
 
-            let nastimobal = document.querySelector('.nastimobal');
-            var nastimclick = document.getElementById('nastimobal');
-            var nastimclickz = document.getElementById('nastimobalz');
-            nastimclick.onclick = function(){
-            nastimobal.classList.toggle('active');
-        };
-            nastimclickz.onclick = function(){
-                nastimobal.classList.remove('active');
-            };
-        
+    nastimobal.classList.toggle('active');
+};
+nastimclickz.onclick = function () {
+    nastimobal.classList.remove('active');
+};
 
-            (function($) {
-                var list = $('.info4');
-                list.find('dd').hide();
 
-                list.find('dt').on('click', function() {
-                    $(this).next().slideToggle()
-                            .siblings('dd').slideUp();
-                });
-            })(jQuery);
+(function ($) {
+    var list = $('.info4');
+    list.find('dd').hide();
 
-            function play() {
-        var audio = document.getElementById('audio');
-        if (audio.paused) {
-            audio.play();
-            $('#play').removeClass('glyphicon-play-circle')
-            $('#play').addClass('glyphicon-pause')
-        }else{
-            audio.pause();
-            audio.currentTime = 0
-            $('#play').addClass('glyphicon-play-circle')
-            $('#play').removeClass('glyphicon-pause')
-        }
+    list.find('dt').on('click', function () {
+        $(this).next().slideToggle()
+            .siblings('dd').slideUp();
+    });
+})(jQuery);
+
+function play() {
+    var audio = document.getElementById('audio');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    } else {
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
     }
+}
 
-    function playvideo() {
-        var video = document.getElementById('uputavka');
-        if (video.paused) {
-            video.play();
-            $('#uputavka').removeClass('glyphicon-play-circle')
-            $('#uputavka').addClass('glyphicon-pause')
-        }else{
-            video.pause();
-            video.currentTime = 0
-            $('#uputavka').addClass('glyphicon-play-circle')
-            $('#uputavka').removeClass('glyphicon-pause')
-        }
-    }
+let videocontainer = document.querySelector('.video-container');
+var uputavkaplay = document.getElementById('uputavka');
+var zatvorBtn = document.getElementById('zatvor-btn');
+var playvideo = document.getElementById('playvideo');
+uputavkaplay.onclick = function () {
+    videocontainer.classList.toggle('active');
+    playvideo.currentTime = 0;
+    document.getElementById('playvideo').play();
+};
+zatvorBtn.onclick = function () {
+    videocontainer.classList.toggle('active');
+    document.getElementById('playvideo').pause();
+};
